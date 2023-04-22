e89f5fb7118163dcb9d28e40d5cd31ca14dd4f31
Connor Brady <connorbrady@codox.eu>
2023-04-22 09:21:58 +0100

    UPDATED Project to use Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AWMSModule } from './awms.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AWMSModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
