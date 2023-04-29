e10abb68106b18bb73a3372cb35928218d453d8b
Connor Brady <connorbrady@codox.eu>
2023-04-29 21:52:34 +0100

    ADDED Keycloak configuration

--- src/awms.module.ts
+++ src/awms.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getAWMSPostgresConnectionSourceOptions } from './ormconfig';
+import { AuthGuard, KeycloakConnectModule } from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...(getAWMSPostgresConnectionSourceOptions() as any),
     }),
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080/',
+      realm: 'awms',
+      clientId: 'admin-cli',
+      secret: '',
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AWMSModule {}
