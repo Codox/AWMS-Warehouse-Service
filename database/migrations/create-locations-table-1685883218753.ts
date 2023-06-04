f2def6f5d796fc1d5b004dd3aa82f82fa11ecf5e
Connor Brady <connorbrady@codox.eu>
2023-06-04 13:58:53 +0100

    ADDED Locations table migration

---
+++ database/migrations/create-locations-table-1685883218753.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateLocationsTable1685883218753 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'locations',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'uuid',
+            type: 'uuid',
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            isUnique: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'width',
+            type: 'float',
+            default: 0,
+          },
+          {
+            name: 'height',
+            type: 'float',
+            default: 0,
+          },
+          {
+            name: 'depth',
+            type: 'float',
+            default: 0,
+          },
+          {
+            name: 'disabled',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('locations');
+  }
+}
