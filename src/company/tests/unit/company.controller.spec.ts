94633a88e7141bd52d4ab29523fdf5cdee86d9fb
Connor Brady <connorbrady@codox.eu>
2023-06-11 21:16:14 +0100

    WIP Setting up Company Controller for testing

--- src/company/tests/unit/company.controller.spec.ts
+++ src/company/tests/unit/company.controller.spec.ts
@@ -1,5 +1,38 @@
-describe('CompanyController', () => {
+import { CompanyController } from '../../../gateway/controllers/company.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CompanyService } from '../../company.service';
+import { EventEmitter2 } from "@nestjs/event-emitter";
 
+describe('CompanyController', () => {
+  let controller: CompanyController;
+  let service: CompanyService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CompanyController],
+      providers: [
+        {
+          provide: CompanyService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: EventEmitter2,
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<CompanyController>(CompanyController);
+    service = module.get<CompanyService>(CompanyService);
+  });
+
+  it('should be defined', async () => {
+    expect(controller).toBeDefined();
+  });
 });
 
 /*
