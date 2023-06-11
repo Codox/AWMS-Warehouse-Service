ce6f0232364da23da14b5a62f48a57a6b45f3cab
Connor Brady <connorbrady@codox.eu>
2023-06-11 21:06:35 +0100

    ADDED First unit test file

---
+++ src/company/tests/unit/company.controller.spec.ts
@@ -0,0 +1,22 @@
+describe('CompanyController', () => {
+
+});
+
+/*
+describe('MyController', () => {
+  let controller: MyController;
+  let service: MyService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [MyController],
+      providers: [MyService],
+    }).compile();
+
+    controller = module.get<MyController>(MyController);
+    service = module.get<MyService>(MyService);
+  });
+
+  // Tests will go here
+});
+*/
