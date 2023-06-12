9f82065573922e411b7a8432c573f161ca9b7421
Connor Brady <connorbrady@codox.eu>
2023-06-12 22:05:26 +0100

    ADDED Test for GET /company

--- src/company/tests/unit/company.controller.spec.ts
+++ src/company/tests/unit/company.controller.spec.ts
@@ -2,19 +2,24 @@ import { CompanyController } from '../../../gateway/controllers/company.controll
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompanyService } from '../../company.service';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Company } from '../../company.entity';
+import { faker } from '@faker-js/faker';
+import { FilterableData } from '../../../shared/filterable-data';
+import { CompanyRepository } from '../../company.repository';
 
 describe('CompanyController', () => {
   let controller: CompanyController;
-  let service: CompanyService;
+  let companyService: CompanyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CompanyController],
       providers: [
+        CompanyService,
         {
-          provide: CompanyService,
+          provide: CompanyRepository,
           useValue: {
-            create: jest.fn(),
+            queryWithFilterable: jest.fn(),
           },
         },
         {
@@ -27,11 +32,52 @@ describe('CompanyController', () => {
     }).compile();
 
     controller = module.get<CompanyController>(CompanyController);
-    service = module.get<CompanyService>(CompanyService);
+    companyService = module.get<CompanyService>(CompanyService);
   });
 
-  it('GET /company should respond correctly', async () => {
+  it('GET /company should resolve correctly', async () => {
+    const mockCompanies: Company[] = [
+      new Company({
+        uuid: faker.string.uuid(),
+        name: faker.company.name(),
+        code: faker.company.name().slice(0, 3).toUpperCase(),
+        description: faker.lorem.paragraph(),
+        vatNumber: faker.finance.accountNumber(),
+        eoriNumber: faker.finance.accountNumber(),
+        contactTelephone: faker.phone.number('+44##########'),
 
+        addressLines: [faker.location.streetAddress({ useFullAddress: true })],
+        town: faker.location.city(),
+        region: faker.location.state(),
+        city: faker.location.city(),
+        zipCode: faker.location.zipCode(),
+        country: faker.location.countryCode(),
+      }),
+    ];
+
+    const baseResponse = {
+      data: mockCompanies,
+      count: mockCompanies.length,
+      page: 5,
+      limit: 10,
+    };
+
+    jest
+      .spyOn(companyService.getRepository(), 'queryWithFilterable')
+      .mockImplementation(async () => baseResponse);
+
+    const filterableData: FilterableData = {
+      fields: [],
+      page: 5,
+      limit: 10,
+    };
+
+    const result = await controller.getCompanies(filterableData);
+
+    expect(result).toEqual(baseResponse);
+    expect(
+      companyService.getRepository().queryWithFilterable,
+    ).toHaveBeenCalledWith(filterableData);
   });
 });
 
