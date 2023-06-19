64fdaa9ffc4ae0c537b9d0fc335dc59eabd933e0
Connor Brady <connorbrady@codox.eu>
2023-06-19 07:45:02 +0100

    ADDED Tests and country lookup functionality

--- src/country/tests/country.controller.spec.ts
+++ src/country/tests/country.controller.spec.ts
@@ -27,10 +27,54 @@ describe('CountryController', () => {
   });
 
   it('GET /country should resolve correctly - 200', async () => {
-
+    const result = await controller.getCounties();
+    expect(result).toContainEqual(testData);
   });
 
-  it('GET /country/alpha/2/:code should resolve correctly - 200', async () => {});
+  it('GET /country/alpha/2/:code should resolve correctly - 200', async () => {
+    const resultUpperCase = await controller.getCountryByAlpha2Code('UA');
+    expect(resultUpperCase).toEqual(testData);
 
-  it('GET /country/:name should not resolve correctly(Not found) - 404', async () => {});
+    const resultNormalCase = await controller.getCountryByAlpha2Code('ua');
+    expect(resultNormalCase).toEqual(testData);
+  });
+
+  it('GET /country/alpha/2/:code should not resolve correctly (Not found) - 404', async () => {
+    await expect(controller.getCountryByAlpha2Code('ZZ')).rejects.toThrow(
+      `Country ZZ not found`,
+    );
+  });
+
+  it('GET /country/:name should resolve correctly - 200', async () => {
+    const resultForNormalCase = await controller.getCountryByName('Ukraine');
+    expect(resultForNormalCase).toEqual(testData);
+
+    const resultForUpperCase = await controller.getCountryByName('UKRAINE');
+    expect(resultForUpperCase).toEqual(testData);
+  });
+
+  it('GET /country/:name should not resolve correctly (Not found) - 404', async () => {
+    await expect(
+      controller.getCountryByName('Rexchoppers Kingdom'),
+    ).rejects.toThrow(`Country Rexchoppers Kingdom not found`);
+  });
+
+  it('Should format country correctly', async () => {
+    const rawDataKey = 'UA';
+
+    const rawDataObject = {
+      name: 'Ukraine',
+      native: 'Україна',
+      phone: '380',
+      continent: 'EU',
+      capital: 'Kyiv',
+      currency: 'UAH',
+      languages: ['uk'],
+      emoji: '🇺🇦',
+      emojiU: 'U+1F1FA U+1F1E6',
+    };
+
+    const result = controller.formatCountry(rawDataKey, rawDataObject);
+    expect(result).toEqual(testData);
+  });
 });
