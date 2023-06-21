8d8c7b05209c3800b756a76e8f9e9899b944add0
Connor Brady <connorbrady@codox.eu>
2023-06-21 22:19:30 +0100

    ADDED Update priority status method in service

--- src/priority-status/dto/priority-status.dto.ts
+++ src/priority-status/dto/priority-status.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PriorityStatusDTO {
--- src/priority-status/priority-status.service.ts
+++ src/priority-status/priority-status.service.ts
@@ -3,6 +3,7 @@ import { BaseService } from '../shared/base.service';
 import { PriorityStatus } from './priority-status.entity';
 import { PriorityStatusRepository } from './priority-status.repository';
 import { PriorityStatusDTO } from './dto/priority-status.dto';
+import { Not } from 'typeorm';
 
 @Injectable()
 export class PriorityStatusService extends BaseService<PriorityStatus> {
@@ -27,5 +28,43 @@ export class PriorityStatusService extends BaseService<PriorityStatus> {
     priorityStatus = await this.priorityStatusRepository.save(priorityStatus);
 
     return priorityStatus;
+  }
+
+  async updatePriorityStatus(
+    uuid: string,
+    data: PriorityStatusDTO,
+  ): Promise<{ old: PriorityStatus; new: PriorityStatus }> {
+    const existingPriorityStatus = await this.getRepository().findOne({
+      where: { uuid },
+    });
+
+    if (!existingPriorityStatus) {
+      throw new BadRequestException(`Priority Status ${uuid} not found`);
+    }
+
+    // Check there are no other priority status with the same name or value
+    const existingPriorityStatusWithSameNameOrValue =
+      await this.getRepository().findOne({
+        where: [
+          { name: data.name, uuid: Not(uuid) },
+          { value: data.value, uuid: Not(uuid) },
+        ],
+      });
+
+    if (existingPriorityStatusWithSameNameOrValue) {
+      throw new BadRequestException(
+        `Conflicting priority status existing with name ${data.name} or value ${data.value}`,
+      );
+    }
+
+    let updatedPriorityStatus = Object.assign(existingPriorityStatus, data);
+    updatedPriorityStatus = await this.priorityStatusRepository.save(
+      updatedPriorityStatus,
+    );
+
+    return {
+      old: existingPriorityStatus,
+      new: updatedPriorityStatus,
+    };
   }
 }
