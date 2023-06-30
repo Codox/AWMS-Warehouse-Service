3c4c65e3122c8d0569dfbfc39abab8e7648d170a
Connor Brady <connorbrady@codox.eu>
2023-06-30 23:33:09 +0100

    ADDED Inverse relation for DangerousGoods

--- src/dangerous-goods/dangerous-goods.entity.ts
+++ src/dangerous-goods/dangerous-goods.entity.ts
@@ -3,12 +3,14 @@ import {
   CreateDateColumn,
   Entity,
   Generated,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { BaseEntity } from '../shared/base.entity';
 import { Exclude } from 'class-transformer';
 import { Mixin } from 'ts-mixer';
+import { DangerousGoodsClassification } from './dangerous-goods-classification.entity';
 
 @Entity('dangerous_goods')
 export class DangerousGoods extends Mixin(BaseEntity) {
@@ -27,6 +29,12 @@ export class DangerousGoods extends Mixin(BaseEntity) {
     name: 'un_class',
   })
   UNClass: string;
+
+  @OneToMany(
+    () => DangerousGoodsClassification,
+    (classification) => classification.dangerousGoods,
+  )
+  classifications: DangerousGoodsClassification[];
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
