c0d35a95896cdc1e22fbf0186d815648f5ee58c2
Connor Brady <connorbrady@codox.eu>
2023-07-01 10:39:28 +0100

    ADDED Get single dangerous goods notification

--- src/dangerous-goods/dangerous-goods.controller.ts
+++ src/dangerous-goods/dangerous-goods.controller.ts
@@ -4,9 +4,11 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  NotFoundException,
+  Param,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { DangerousGoodsService } from './dangerous-goods.service';
 import { DangerousGoodsClassificationService } from './dangerous-goods-classification.service';
 import { Roles } from 'nest-keycloak-connect';
@@ -27,5 +29,32 @@ export class DangerousGoodsController {
     return await this.dangerousGoodsService.getRepository().find({
       relations: ['classifications'],
     });
+  }
+
+  @Get('/:uuid')
+  @Roles({ roles: ['realm:super_admin'] })
+  @HttpCode(HttpStatus.OK)
+  @ApiParam({
+    name: 'uuid',
+    type: String,
+    required: true,
+
+    description: 'Dangerous Goods UUID',
+  })
+  async getDangerousGoods(@Param('uuid') uuid: string) {
+    const dangerousGoods = await this.dangerousGoodsService
+      .getRepository()
+      .findOne({
+        where: { uuid },
+        relations: ['classifications'],
+      });
+
+    if (!dangerousGoods) {
+      throw new NotFoundException(`Dangerous Goods ${uuid} not found`);
+    }
+
+    return {
+      data: dangerousGoods,
+    };
   }
 }
