d4d4ad06af1b334aaac59746e2459411698cb84c
Connor Brady <connorbrady@codox.eu>
2023-07-03 20:06:56 +0100

    FIXED EventEmitter dependency

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { CompanyRepository } from './company.repository';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
+import { DatabaseModule } from '../database/database.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
+  imports: [DatabaseModule, EventEmitterModule.forRoot()],
   controllers: [CompanyController],
   providers: [CompanyService, CompanyRepository],
   exports: [CompanyService],
