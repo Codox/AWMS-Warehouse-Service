cf505a07a163fac105c4698510bf7837579641d4
Connor Brady <connorbrady@codox.eu>
2023-07-04 21:18:22 +0100

    ADDED Passing environment variables into E2E tests

--- test/e2e/run-local.sh
+++ test/e2e/run-local.sh
@@ -62,21 +62,23 @@ fi
 KEYCLOAK_CONTAINER_ID=$(docker-compose ps -q keycloak)
 
 docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh config credentials --config=/opt/bitnami/keycloak/.kcadm.config --server=http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm=master --user=test --password=test
-docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh create clients --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master -s clientId=test -s enabled=true -s publicClient=false -s directAccessGrantsEnabled=true -s clientAuthenticatorType=client-secret
-docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh get clients/test/client-secret --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master
+CREATE_CLIENT_RESPONSE=$(docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh create clients --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master -s clientId=test -s enabled=true -s publicClient=false -s directAccessGrantsEnabled=true -s clientAuthenticatorType=client-secret 2>&1)
+CLIENT_UUID=$(echo "$CREATE_CLIENT_RESPONSE" | awk -F"'" '{print $2}')
 
+CLIENT_SECRET_RESPONSE=$(docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh get clients/$CLIENT_UUID/client-secret --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master 2>&1)
+CLIENT_SECRET=$(echo "$CLIENT_SECRET_RESPONSE" | awk -F'"' '/value/ {print $4}')
 
-# CD to main code directory
+# cd to main code directory
 cd ../..
 
 # Run migrations
 npm run migrate:run
 
 # Run the tests
-./node_modules/.bin/jest --maxWorkers=50% --config jest-e2e.json
+env KEYCLOAK_URL=http://$KEYCLOAK_HOST:$KEYCLOAK_EXTERNAL_PORT KEYCLOAK_REALM=master KEYCLOAK_CLIENT_ID=test KEYCLOAK_CLIENT_SECRET=$CLIENT_SECRET ./node_modules/.bin/jest --maxWorkers=50% --config jest-e2e.json
 
 # Move back to E2E tests directory
 cd test/e2e
 
 # Tear down the containers
-# docker-compose down -v
+docker-compose down -v
