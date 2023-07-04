7f85cb79599269933da743feb6a44eb97d3090d5
Connor Brady <connorbrady@codox.eu>
2023-07-04 22:41:26 +0100

    ADDED Testing service for utilities to run E2E tests

---
+++ src/shared/test/e2e/e2e-testing.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class E2ETestingService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getAccessToken() {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post<any>(
+          'http://localhost:6080/realms/master/protocol/openid-connect/token',
+          {
+            grant_type: 'password',
+            client_id: process.env.KEYCLOAK_CLIENT_ID,
+            client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+            username: 'test',
+            password: 'test',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          },
+        )
+        .pipe(
+          catchError((error) => {
+            console.log(error);
+            // this.logger.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    return data.access_token;
+
+    const response = await this.httpService
+      .post(
+        'http://localhost:6080/auth/realms/master/protocol/openid-connect/token',
+        {
+          grant_type: 'password',
+          client_id: process.env.KEYCLOAK_CLIENT_ID,
+          client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+          username: 'test',
+          password: 'test',
+        },
+      )
+      .toPromise();
+
+    return response.data.access_token;
+  }
+}
+
+/*export async function getAccessToken(app: NestFastifyApplication) {
+  const http = app.getHttpAdapter().getInstance();
+
+  const response = await http.post({
+    url: 'http://localhost:6080/auth/realms/master/protocol/openid-connect/token',
+  });
+
+  console.log(response);
+}*/
