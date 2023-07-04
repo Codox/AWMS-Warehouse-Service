376fe12b9ef6a45b385a9731e4dbc0056590e8da
Connor Brady <connorbrady@codox.eu>
2023-07-04 20:39:41 +0100

    WIP Setting up clients

--- test/e2e/run-local.sh
+++ test/e2e/run-local.sh
@@ -6,8 +6,10 @@ export DB_USER=test
 export DB_NAME=test
 export DB_PASS=test
 
+# Local information for Keycloak as this command will be ran in the container itself
 export KEYCLOAK_HOST=localhost
-export KEYCLOAK_PORT=6080
+export KEYCLOAK_INTERNAL_PORT=8080
+export KEYCLOAK_EXTERNAL_PORT=6080
 
 # Set the following to enable retries with a delay between them
 MAX_RETRIES=10
@@ -31,17 +33,38 @@ else
   exit 1
 fi
 
+# Function to make an HTTP request and check the response status
+check_keycloak_status() {
+  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "http://$KEYCLOAK_HOST:$KEYCLOAK_EXTERNAL_PORT/realms/master")
+  if [ "$RESPONSE" = "200" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 # Wait for Keycloak to be ready
 RETRIES=0
-until nc -z "$KEYCLOAK_HOST" "$KEYCLOAK_PORT" || [ $RETRIES -eq $MAX_RETRIES ]; do
+until check_keycloak_status || [ $RETRIES -eq $MAX_RETRIES ]; do
   RETRIES=$((RETRIES + 1))
-  echo "Waiting for Keycloak service to be up..."
+  echo "Waiting for Keycloak service to be up... (Attempt $RETRIES)"
   sleep $RETRY_DELAY
 done
 
-# Setup Keylcoak information for E2E testing
+if check_keycloak_status; then
+  echo "Keycloak is ready"
+else
+  echo "Keycloak is not ready"
+  exit 1
+fi
+
+# Setup Keycloak information for E2E testing
 KEYCLOAK_CONTAINER_ID=$(docker-compose ps -q keycloak)
-docker exec "$KEYCLOAK_CONTAINER_ID" echo "Keycloak is up and running"
+
+docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh config credentials --config=/opt/bitnami/keycloak/.kcadm.config --server=http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm=master --user=test --password=test
+docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh create clients --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master -s clientId=test -s enabled=true -s publicClient=false -s directAccessGrantsEnabled=true -s clientAuthenticatorType=client-secret
+docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh get clients/test/client-secret --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master
+
 
 # CD to main code directory
 cd ../..
@@ -56,4 +79,4 @@ npm run migrate:run
 cd test/e2e
 
 # Tear down the containers
-docker-compose down
+# docker-compose down -v
