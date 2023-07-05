6601b19c87470d8e896f5930ea70071ec1149014
Connor Brady <connorbrady@codox.eu>
2023-07-05 08:38:39 +0100

    UPDATED Company module imports

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -4,11 +4,23 @@ import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { DatabaseModule } from '../database/database.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import {
+  getEndpointProtectionProviders,
+  getKeycloakConfiguration,
+} from '../shared/endpoint-protection';
 
 @Module({
-  imports: [DatabaseModule, EventEmitterModule.forRoot()],
+  imports: [
+    DatabaseModule,
+    EventEmitterModule.forRoot(),
+    getKeycloakConfiguration(),
+  ],
   controllers: [CompanyController],
-  providers: [CompanyService, CompanyRepository],
+  providers: [
+    CompanyService,
+    CompanyRepository,
+    ...getEndpointProtectionProviders(),
+  ],
   exports: [CompanyService],
 })
 export class CompanyModule {}
