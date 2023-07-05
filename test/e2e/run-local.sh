fe0b216f00a37c9d5a893d5e9c7b7f8b2462b8b0
Connor Brady <connorbrady@codox.eu>
2023-07-05 10:07:30 +0100

    UPDATED Script to add user to super_admin realm role
    This will be expanded later on, but this is to get the tests working for now

--- test/e2e/run-local.sh
+++ test/e2e/run-local.sh
@@ -68,6 +68,14 @@ CLIENT_UUID=$(echo "$CREATE_CLIENT_RESPONSE" | awk -F"'" '{print $2}')
 CLIENT_SECRET_RESPONSE=$(docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh get clients/$CLIENT_UUID/client-secret --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master 2>&1)
 CLIENT_SECRET=$(echo "$CLIENT_SECRET_RESPONSE" | awk -F'"' '/value/ {print $4}')
 
+GET_USER_RESPONSE=$(docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh get users --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --query "username=test" --fields id 2>&1 --realm master 2>&1)
+USER_UUID=$(echo "$GET_USER_RESPONSE" | awk -F'"' '{print $4}')
+
+docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh create roles --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master -s name=super_admin
+docker exec --user keycloak "$KEYCLOAK_CONTAINER_ID" /opt/bitnami/keycloak/bin/kcadm.sh add-roles --config /opt/bitnami/keycloak/.kcadm.config --server http://$KEYCLOAK_HOST:$KEYCLOAK_INTERNAL_PORT --realm master --uusername test --rolename super_admin
+
+
+
 # cd to main code directory
 cd ../..
 
