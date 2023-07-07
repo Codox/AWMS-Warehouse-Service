00cadbf92b7132679f32734fa89f2764f63098ab
Connor Brady <connorbrady@codox.eu>
2023-07-07 16:48:39 +0100

    FIXED URL Issue with E2E tests

--- src/shared/test/e2e/e2e-testing.service.ts
+++ src/shared/test/e2e/e2e-testing.service.ts
@@ -10,7 +10,7 @@ export class E2ETestingService {
     const { data } = await firstValueFrom(
       this.httpService
         .post<any>(
-          `http://127.0.0.1:6080/realms/master/protocol/openid-connect/token`,
+          `${process.env.KEYCLOAK_URL}/realms/master/protocol/openid-connect/token`,
           {
             grant_type: 'password',
             client_id: process.env.KEYCLOAK_CLIENT_ID,
@@ -34,21 +34,6 @@ export class E2ETestingService {
     );
 
     return data.access_token;
-
-    const response = await this.httpService
-      .post(
-        'http://localhost:6080/auth/realms/master/protocol/openid-connect/token',
-        {
-          grant_type: 'password',
-          client_id: process.env.KEYCLOAK_CLIENT_ID,
-          client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
-          username: 'test',
-          password: 'test',
-        },
-      )
-      .toPromise();
-
-    return response.data.access_token;
   }
 }
 
