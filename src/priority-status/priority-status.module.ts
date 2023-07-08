2b4580d4a9e2899ba80b5bd34b951123efb2ca99
Connor Brady <connorbrady@codox.eu>
2023-07-08 10:21:51 +0100

    ADDED E2E Tests in priority status

--- src/priority-status/priority-status.module.ts
+++ src/priority-status/priority-status.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { PriorityStatusRepository } from './priority-status.repository';
 import { PriorityStatusService } from './priority-status.service';
 import { PriorityStatusController } from './priority-status.controller';
+import { DatabaseModule } from '../database/database.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import {
+  getEndpointProtectionProviders,
+  getKeycloakConfiguration,
+} from '../shared/endpoint-protection';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    EventEmitterModule.forRoot(),
+    getKeycloakConfiguration(),
+  ],
   controllers: [PriorityStatusController],
-  providers: [PriorityStatusService, PriorityStatusRepository],
+  providers: [
+    PriorityStatusService,
+    PriorityStatusRepository,
+    ...getEndpointProtectionProviders(),
+  ],
   exports: [PriorityStatusService],
 })
 export class PriorityStatusModule {}
