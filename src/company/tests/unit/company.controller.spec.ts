18ff8f168661dc494ac18024dc06a96ed00bd906
Connor Brady <connorbrady@codox.eu>
2023-07-08 13:17:21 +0100

    WIP Optimising tests and extracting common functionality

--- src/company/tests/unit/company.controller.spec.ts
+++ src/company/tests/unit/company.controller.spec.ts
@@ -7,6 +7,14 @@ import { FilterableData } from '../../../shared/filterable-data';
 import { CompanyRepository } from '../../company.repository';
 import { KeycloakUser } from '../../../user/keycloak-user';
 import { CompanyController } from '../../company.controller';
+import {
+  createCompany,
+  createKeycloakUser,
+  expectFilterableCalledCorrectly,
+  mockFindOne,
+  mockQueryWithFilterable,
+  responseToBeCorrect,
+} from '../../../shared/test/test-utilities';
 
 describe('CompanyController', () => {
   let controller: CompanyController;
@@ -40,35 +48,14 @@ describe('CompanyController', () => {
   });
 
   it('GET /company should resolve correctly - 200', async () => {
-    const mockCompanies: Company[] = [
-      new Company({
-        uuid: faker.string.uuid(),
-        name: faker.company.name(),
-        code: faker.company.name().slice(0, 3).toUpperCase(),
-        description: faker.lorem.paragraph(),
-        vatNumber: faker.finance.accountNumber(),
-        eoriNumber: faker.finance.accountNumber(),
-        contactTelephone: faker.phone.number('+44##########'),
+    const mockCompanies: Company[] = [createCompany()];
 
-        addressLines: [faker.location.streetAddress({ useFullAddress: true })],
-        town: faker.location.city(),
-        region: faker.location.state(),
-        city: faker.location.city(),
-        zipCode: faker.location.zipCode(),
-        country: faker.location.countryCode(),
-      }),
-    ];
-
-    const baseResponse = {
-      data: mockCompanies,
-      count: mockCompanies.length,
-      page: 5,
-      limit: 10,
-    };
-
-    jest
-      .spyOn(companyService.getRepository(), 'queryWithFilterable')
-      .mockImplementation(async () => baseResponse);
+    mockQueryWithFilterable(
+      companyService.getRepository(),
+      mockCompanies,
+      5,
+      10,
+    );
 
     const filterableData: FilterableData = {
       fields: [],
@@ -78,41 +65,28 @@ describe('CompanyController', () => {
 
     const result = await controller.getCompanies(filterableData);
 
-    expect(result).toEqual(baseResponse);
-    expect(
-      companyService.getRepository().queryWithFilterable,
-    ).toHaveBeenCalledWith(filterableData);
+    expectFilterableCalledCorrectly(
+      result,
+      companyService.getRepository(),
+      {
+        data: mockCompanies,
+        count: mockCompanies.length,
+        page: 5,
+        limit: 10,
+      },
+      filterableData,
+    );
   });
 
   it('GET /company/:uuid should resolve correctly - 200', async () => {
-    const company = new Company({
-      uuid: faker.string.uuid(),
-      name: faker.company.name(),
-      code: faker.company.name().slice(0, 3).toUpperCase(),
-      description: faker.lorem.paragraph(),
-      vatNumber: faker.finance.accountNumber(),
-      eoriNumber: faker.finance.accountNumber(),
-      contactTelephone: faker.phone.number('+44##########'),
+    const company = createCompany();
 
-      addressLines: [faker.location.streetAddress({ useFullAddress: true })],
-      town: faker.location.city(),
-      region: faker.location.state(),
-      city: faker.location.city(),
-      zipCode: faker.location.zipCode(),
-      country: faker.location.countryCode(),
-    });
-
-    const baseResponse = {
-      data: company,
-    };
-
-    jest
-      .spyOn(companyService.getRepository(), 'findOne')
-      .mockImplementation(async () => company);
+    mockFindOne(companyService.getRepository(), company);
 
     const result = await controller.getCompany(company.uuid);
 
-    expect(result).toEqual(baseResponse);
+    responseToBeCorrect(result, company);
+
     expect(companyService.getRepository().findOne).toHaveBeenCalledWith({
       where: { uuid: company.uuid },
     });
@@ -134,27 +108,9 @@ describe('CompanyController', () => {
   });
 
   it('POST /company should resolve correctly - 201', async () => {
-    const company = new Company({
-      uuid: faker.string.uuid(),
-      name: faker.company.name(),
-      code: faker.company.name().slice(0, 3).toUpperCase(),
-      description: faker.lorem.paragraph(),
-      vatNumber: faker.finance.accountNumber(),
-      eoriNumber: faker.finance.accountNumber(),
-      contactTelephone: faker.phone.number('+44##########'),
+    const company = createCompany();
 
-      addressLines: [faker.location.streetAddress({ useFullAddress: true })],
-      town: faker.location.city(),
-      region: faker.location.state(),
-      city: faker.location.city(),
-      zipCode: faker.location.zipCode(),
-      country: faker.location.countryCode(),
-    });
-
-    const user: KeycloakUser = {
-      sub: faker.string.uuid(),
-      email: faker.internet.email(),
-    };
+    const user: KeycloakUser = createKeycloakUser();
 
     const baseResponse = {
       data: company,
