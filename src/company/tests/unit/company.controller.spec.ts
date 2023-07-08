0f2032b77fbbf65239450b09aab52da8cb9dca20
Connor Brady <connorbrady@codox.eu>
2023-07-08 13:52:44 +0100

    UPDATED Unit tests for Company

--- src/company/tests/unit/company.controller.spec.ts
+++ src/company/tests/unit/company.controller.spec.ts
@@ -3,18 +3,22 @@ import { CompanyService } from '../../company.service';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Company } from '../../company.entity';
 import { faker } from '@faker-js/faker';
-import { FilterableData } from '../../../shared/filterable-data';
 import { CompanyRepository } from '../../company.repository';
 import { KeycloakUser } from '../../../user/keycloak-user';
 import { CompanyController } from '../../company.controller';
 import {
   createCompany,
   createKeycloakUser,
+  expectEventEmitted,
+  expectExceptionToBeThrown,
   expectFilterableCalledCorrectly,
+  expectFilterableResponseToBeCorrect,
+  expectFindOneCalledWithUUID,
+  expectResponseToBeCorrect,
   mockFindOne,
   mockQueryWithFilterable,
-  responseToBeCorrect,
 } from '../../../shared/test/test-utilities';
+import { BadRequestException } from '@nestjs/common';
 
 describe('CompanyController', () => {
   let controller: CompanyController;
@@ -57,25 +61,18 @@ describe('CompanyController', () => {
       10,
     );
 
-    const filterableData: FilterableData = {
+    const result = await controller.getCompanies({
       fields: [],
       page: 5,
       limit: 10,
-    };
+    });
 
-    const result = await controller.getCompanies(filterableData);
-
-    expectFilterableCalledCorrectly(
-      result,
-      companyService.getRepository(),
-      {
-        data: mockCompanies,
-        count: mockCompanies.length,
-        page: 5,
-        limit: 10,
-      },
-      filterableData,
-    );
+    expectFilterableCalledCorrectly(result, companyService.getRepository(), {
+      fields: [],
+      page: 5,
+      limit: 10,
+    });
+    expectFilterableResponseToBeCorrect(result, mockCompanies, 5, 10);
   });
 
   it('GET /company/:uuid should resolve correctly - 200', async () => {
@@ -83,52 +80,41 @@ describe('CompanyController', () => {
 
     mockFindOne(companyService.getRepository(), company);
 
-    const result = await controller.getCompany(company.uuid);
-
-    responseToBeCorrect(result, company);
-
-    expect(companyService.getRepository().findOne).toHaveBeenCalledWith({
-      where: { uuid: company.uuid },
-    });
+    expectResponseToBeCorrect(
+      await controller.getCompany(company.uuid),
+      company,
+    );
+    expectFindOneCalledWithUUID(companyService.getRepository(), company.uuid);
   });
 
   it('GET /company/:uuid should not resolve (Not found) - 404', async () => {
     const uuid = faker.string.uuid();
 
-    jest
-      .spyOn(companyService.getRepository(), 'findOne')
-      .mockImplementation(async () => undefined);
+    mockFindOne(companyService.getRepository(), undefined);
 
-    await expect(controller.getCompany(uuid)).rejects.toThrow(
-      `Company ${uuid} not found`,
+    await expectExceptionToBeThrown(
+      controller.getCompany(uuid),
+      new BadRequestException(`Company ${uuid} not found`),
     );
-    expect(companyService.getRepository().findOne).toHaveBeenCalledWith({
-      where: { uuid },
-    });
+    expectFindOneCalledWithUUID(companyService.getRepository(), uuid);
   });
 
   it('POST /company should resolve correctly - 201', async () => {
     const company = createCompany();
-
     const user: KeycloakUser = createKeycloakUser();
-
-    const baseResponse = {
-      data: company,
-    };
 
     jest
       .spyOn(companyService, 'createCompany')
       .mockImplementation(async () => company);
 
-    const result = await controller.createCompany(company, user);
-
-    expect(result).toEqual(baseResponse);
-    expect(eventEmitter.emit).toHaveBeenCalledWith(
-      'company.created',
-      expect.objectContaining({
-        companyUuid: company.uuid,
-        userUuid: user.sub,
-      }),
+    expectResponseToBeCorrect(
+      await controller.createCompany(company, user),
+      company,
     );
+
+    expectEventEmitted(eventEmitter, 'company.created', {
+      companyUuid: company.uuid,
+      userUuid: user.sub,
+    });
   });
 });
