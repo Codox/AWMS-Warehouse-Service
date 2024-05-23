e3bf9f223d2723b597e60a9a062bc83d8354dbe8
rexchoppers <connorbrady@codox.uk>
2024-05-23 14:35:08 +0100

    WIP Database and migrations setup

--- cmd/awms-be/main.go
+++ cmd/awms-be/main.go
@@ -2,28 +2,75 @@ package main
 
 import (
 	"awms-be/internal/config"
-	"awms-be/internal/routes"
-	"context"
-	"fmt"
+	"database/sql"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"os"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	gormPg "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
 )
+
+var db *gorm.DB
 
 func main() {
 	cfg := config.Load()
 
 	// Connect to Postgres
-	pool, err := pgxpool.New(context.Background(), cfg.PostgresURI)
+	db, err := gorm.Open(gormPg.Open(cfg.PostgresURI), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
-	defer pool.Close()
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Running migrations - before")
+
+	runMigrations(sqlDB)
+
+	log.Printf("Running migrations - after")
 
 	r := gin.Default()
 
-	routes.SetupRoutes(r, pool)
+	// routes.SetupRoutes(r, pool)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func runMigrations(db *sql.DB) {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	log.Printf("Running migrations")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"postgres", driver)
+
+	log.Printf("Running migrations - after new instance")
+
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+	m.Up()
+	log.Printf("Running migrations - RUN")
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalln(err)
+	}
 }
