e7872e6dbb352f5c4c3e6dd1ccaaa980b46490f4
Connor Brady <connorbrady@codox.eu>
2024-09-02 08:19:17 +0100

    UPDATED Port to stop conflict

--- cmd/awms-be/main.go
+++ cmd/awms-be/main.go
@@ -53,7 +53,7 @@ func main() {
 
 	routes.SetupRoutes(r, db)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func runMigrations(db *sql.DB) {
